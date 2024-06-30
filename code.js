3f5af81bdf159b0078f10c040ed5a161c78f70c9
$@#il <sahilsadekar249775@gmail.com>
2024-06-30 10:48:48 +0530

    deployment commit

---
+++ code.js
@@ -0,0 +1,6 @@
+var typed=new Typed(".typing",{
+    strings:["Web Developer","Android Developer","Java Developer","Web Designer","Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
